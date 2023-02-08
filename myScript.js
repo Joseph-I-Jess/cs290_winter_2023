6d6a384f16624136908cdd855027689d88f39543
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2023-02-08 11:48:02 -0800

    Add JavaScript basics.

---
+++ myScript.js
@@ -0,0 +1,6 @@
+function changeText() {
+    const title = document.getElementById("title");
+    title.innerHTML = "JavaScript is Cool!";
+    alert("hi there!")
+    console.log("Click on button object has occurred.");
+  }
